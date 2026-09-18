aa68cc2b42dffd3c751535a97839365d5a3f580e
OisinStapleton-STUDE <G00365445@atu.ie>
2026-09-18 10:25:46 +0100

    Added Post and Get for Product Controller

--- src/main/java/ie/atu/catalog/controller/ProductController.java
+++ src/main/java/ie/atu/catalog/controller/ProductController.java
@@ -1,4 +1,25 @@
 package ie.atu.catalog.controller;
 
+import ie.atu.catalog.model.Product;
+import ie.atu.catalog.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+@GetMapping
+    public List<Product> getProducts() {
+        return productService.getAll();
+    }
+@PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.create(product);
+    }
 }
